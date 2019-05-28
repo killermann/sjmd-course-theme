0d89ca1a02bbf4b80059dd961302738de5e00d85
Sam Killermann <sK@hues.xyz>
2019-05-28 14:30:59 -0500

    more ld 3.0 tweaks

--- functions.php
+++ functions.php
@@ -147,3 +147,20 @@ function remove_admin_bar() {
 // remove_menu_page('users.php'); // Users
 // remove_menu_page('tools.php'); // Tools
 // remove_menu_page('options-general.php'); // Settings
+
+
+// Disable support for comments and trackbacks in post types
+function df_disable_comments_post_types_support() {
+	$post_types = get_post_types();
+	foreach ($post_types as $post_type) {
+		if(post_type_supports($post_type, 'comments')) {
+			remove_post_type_support($post_type, 'comments');
+			remove_post_type_support($post_type, 'trackbacks');
+		}
+	}
+}
+add_action('admin_init', 'df_disable_comments_post_types_support');
+// Close comments on the front-end
+function df_disable_comments_status() {
+	return false;
+}
